1406bf60b724d0122d7334bec96445fd65fc7444
Pedro Mesquita <devpedromesquita@gmail.com>
2025-09-23 17:42:20 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,80 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//-------------------------------------------------------------------------
+// Lista que vai armazenar os nomes
+let amigos = [];
+let contador = 0;
+//-------------------------------------------------------------------------
+// Função para adicionar amigo
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome !== "") {
+
+        if(amigos.length == 0){
+           amigos.push(nome); 
+        }else{
+            //Verifica se o nome já está na lista]
+            while(contador <= amigos.length - 1){
+                if(nome === amigos[contador]){
+                    let valor = prompt("Nome já esta na lista desejá adicionalo novamente? (s = sim / n = não)");
+
+                    if (valor == 'S' || valor == 's'){
+                        amigos.push(nome);
+                    }else{
+                        alert("Nome não adicionado")
+                    }
+                }else{
+                    amigos.push(nome);
+                }
+
+            }
+            
+        }
+
+
+
+        // Atualiza a lista no HTML
+        atualizarLista();
+
+        // limpa o campo
+        input.value = "";
+    } else {
+        alert("Digite um nome válido!");
+    }
+}
+
+//-------------------------------------------------------------------------
+// Atualiza a <ul> com os nomes
+function atualizarLista() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = ""; // limpa antes de atualizar
+
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        ul.appendChild(li);
+    });
+}
+
+//-------------------------------------------------------------------------
+// Função para sortear um amigo secreto
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Adicione pelo menos um amigo!");
+    return;
+  }
+
+  let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>🎉 O amigo secreto sorteado é: <strong>${sorteado}</strong></li>`;
+}
+
+//-------------------------------------------------------------------------
+// FUnção para limpar a lista 
+function limparLista() {
+  amigos = []; // esvazia o array
+  document.getElementById("listaAmigos").innerHTML = ""; // limpa a lista na tela
+  document.getElementById("resultado").innerHTML = "";   // limpa o resultado também
+}
